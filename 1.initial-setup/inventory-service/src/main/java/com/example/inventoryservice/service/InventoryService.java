5a0aa0aa9098d2556638a0a382e191fe099a8ad5
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-06-10 00:04:18 +0530

    Initial Setup

---
+++ 1.initial-setup/inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.example.inventoryservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.inventoryservice.repository.InventoryRepository;
+
+@Service
+public class InventoryService {
+
+	@Autowired
+	private InventoryRepository inventoryRepository;
+	
+	@Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
---
+++ 1.initial-setup/order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.example.orderservice.service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.orderservice.dto.OrderLineItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public void placeOrder(OrderRequest orderRequest) {
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
